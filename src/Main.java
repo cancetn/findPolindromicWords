9712703463ad7977a0847b01c635dfadac2fa524
Can Cetin <cancetn@hotmail.com>
2022-08-04 12:30:57 +0300

    Patika.dev JAVA Egitim Proje Odevi

---
+++ src/Main.java
@@ -0,0 +1,39 @@
+public class Main {
+
+    //Patika.dev JAVA Egitim Proje Odevi
+
+
+
+    /*
+     Java dilinde kullanıcının girdiği kelimenin "Palindromik" olup olmadığı bulan bir program yazıyoruz.
+
+     Eğer bir kelimenin tersten okunuşu yine aynı kelimeyi veriyorsa o kelime "Palindromik Kelime'dir".
+
+     Örnek : abba , asa , kavak, kayak
+     */
+
+    static boolean isPalindrome(String str){
+        int i =0;
+        int j = str.length()-1;
+
+        while (i<j){
+
+            if(str.charAt(i) != str.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+
+        }
+        return true;
+    }
+
+
+
+    public static void main(String[] args) {
+
+        System.out.println(isPalindrome("abba"));
+
+
+    }
+}
